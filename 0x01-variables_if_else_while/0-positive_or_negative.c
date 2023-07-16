ca8545b8b62784115ade8186e06b8ec4c86e5cb9
Mohd-Abdallah <dawry798@gmail.com>
2023-07-15 17:11:35 -0700

    Fixed Betty Style error, Task No.0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * This program will assign a random number to the variable n
- * each time it is executed.
+
+/**
+ * main - will assign a random number to the variable n
+ * each time it is executed
+ *
+ * Return: Always 0 (Success)
  */
 
-/*
- * main - this function generates a random number
- * then checks if it's positive or negative.
- *
- * Return: 0 at the end
-*/
 int main(void)
 {
 	int n;
