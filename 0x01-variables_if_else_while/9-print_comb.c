4645b13a17a47645db30d8c8af950408df47cb5f
Mohd-Abdallah <dawry798@gmail.com>
2023-07-15 20:59:26 -0700

    Hotfix 6, Task No.9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(i + '0');
 	return (0);
 }
