c121a3504f72dcb69f2f7bd20b039d7b20667bc5
Mohd-Abdallah <dawry798@gmail.com>
2023-07-19 19:12:09 -0700

    Hotfix Task No.0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,14 @@
  */
 int main(void)
 {
-	char alph = "_putchar";
-
-	int _putchar(alph);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
