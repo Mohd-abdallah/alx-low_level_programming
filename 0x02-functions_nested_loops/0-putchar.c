f669a19664a7812d58107fbe3dc379856bb8e106
Mohd-Abdallah <dawry798@gmail.com>
2023-07-19 18:52:00 -0700

    Hotfix Task No.0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,11 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main()
 {
-	return (write(1, &c, 1));
+	int _putchar(char);
+
+	char alph = "_putchar";
+	int _putchar(alph);
+	return (0);
 }
