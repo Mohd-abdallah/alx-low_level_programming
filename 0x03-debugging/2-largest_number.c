be0af98b096f4cba5c9b2e4960ef3e63727304db
Mohd-Abdallah <dawry798@gmail.com>
2023-07-23 19:05:36 -0700

    Task No.2 Hotfix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,21 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+	else if (a > b && b > c)
+	{
+		largest = a;
+	}
 	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
