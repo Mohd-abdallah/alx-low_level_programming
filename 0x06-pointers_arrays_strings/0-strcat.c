416fb834980e6e36d4dbec55f3ea71a31f609ce3
Mohd-Abdallah <dawry798@gmail.com>
2023-07-27 14:52:23 -0700

    New Project & Task No.0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings
+ *
+ * @dest: string input
+ * @src: string input
+ *
+ * Return: concatenated string
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
