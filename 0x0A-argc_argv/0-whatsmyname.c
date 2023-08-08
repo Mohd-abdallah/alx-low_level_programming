6be66233c570953d87807550f1e69490970e3fa2
Mohd-Abdallah <dawry798@gmail.com>
2023-08-08 16:58:39 -0700

    Task No.0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: input
+ * @argv: array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (i == 6)
+	{
+		printf("%s", argv[i]);
+	}
+	else
+	{
+		i++;
+	}
+	return (0);
+}
