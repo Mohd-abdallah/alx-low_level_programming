63351acba9046973c139c02d9f5dc86f53382ee6
Mohd-Abdallah <dawry798@gmail.com>
2023-08-08 18:26:25 -0700

    Task No.3 Hotfix 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,15 @@
 int main(int argc, char *argv[])
 {
 	int multi;
-	int num1;
-	int num2;
 
 	if (argc < 3 || argc > 3)
 	{
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		multi = num1 * num2;
+		multi = argv[1] * argv[2];
 		printf("%d\n", multi);
 	}
 	return (0);
