e5c02d061e934b77e9edab45b4c297a245149677
Mohd-Abdallah <dawry798@gmail.com>
2023-08-11 16:48:16 -0700

    Task No.0 Hotfix 3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *str = malloc(size * sizeof(char));
+	char *str = malloc(size * sizeof(str));
 	unsigned int i;
 
 	if (size == 0 || str == NULL)
