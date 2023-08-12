e65514064040e79592f0194711475861bcb99382
Mohd-Abdallah <dawry798@gmail.com>
2023-08-12 11:13:21 -0700

    Task No.2 Hotfix 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,7 @@ char *str_concat(char *s1, char *s2)
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
 	int len = len1 + len2 + 1;
+	char *new_srt;
 
 	if (s1 == NULL) {
 		s1 = "";
@@ -26,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL) {
 		s2 = "";
 	}
-	char *new_str = malloc(len * sizeof(char));
+	new_str = malloc(len * sizeof(char));
 	if (new_str == NULL) {
 		return (NULL);
 	}
