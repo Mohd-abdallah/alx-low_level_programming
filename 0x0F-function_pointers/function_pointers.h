7fe81347e6bdfd2ca19c0c1f4fc9a7f3194de592
Mohd-Abdallah <dawry798@gmail.com>
2023-08-16 11:56:57 -0700

    Task No.0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+
+#endif
