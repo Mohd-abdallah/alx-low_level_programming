2d1b22f44c66ee13603fec56556e00b726ccff86
Mohd-Abdallah <dawry798@gmail.com>
2023-08-16 14:49:42 -0700

    Task No.2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,5 +7,11 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
 void print_elem_hex(int elem);
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+
+
 #endif
