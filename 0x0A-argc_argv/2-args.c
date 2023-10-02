eca842c39f0d8d11f366ad5d9bb0ab719260b044
Mohd-Abdallah <dawry798@gmail.com>
2023-10-02 21:21:52 +0200

    fixing betty error = Task No.2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * main -  a program that prints all arguments it receives.
  * All arguments should be printed, including the first one.
  * Only print one argument per line, ending with a new line.
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <= argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
