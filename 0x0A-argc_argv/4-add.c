0465d4d65c9736f58cd40bcdb9dbf18cefbb576f
Mohd-Abdallah <dawry798@gmail.com>
2023-10-03 00:12:44 +0200

    Hotfix 2 - Task No.4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,18 +27,18 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			for (j = 0; argv[i][j] != 0; j++)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (argv[i][j] < 0 || argv[i][j] > 9)
+				if (argv[i][j] < '0' || argv[i][j] > '9')
 				{
 					printf("Error\n");
 					return (1);
 				}
 			}
 			add += atoi(argv[i]);
-			printf("%d\n", add);
 		}
 	}
+	printf("%d\n", add);
 	return (0);
 }
 
