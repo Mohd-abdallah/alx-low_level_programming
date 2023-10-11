1c756218e6f7880e7d6f2a9b6f4228b2f20b3247
Mohd-Abdallah <dawry798@gmail.com>
2023-10-12 01:05:38 +0200

    updating dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+/* Structures */
+
 /**
  * struct dog - new struct
  * @name: char.
@@ -13,5 +15,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+
+
+
+/* Prototypes */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
