ca0ee738e888a7f7eb4194f4078771db433c1c51
Mohd-Abdallah <dawry798@gmail.com>
2023-10-12 01:39:40 +0200

    modifying dog.h file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if ((*d).name == NULL)
+	if (d->name == NULL)
 	{
 		printf("Name: (nil)\n");
 	}
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if ((*d).age < 0)
+	if (d->age < 0)
 	{
 		printf("Age: (nill)\n");
 	}
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Age: %f\n", d->age);
 	}
-	if ((*d).owner == NULL)
+	if (d->owner == NULL)
 	{
 		printf("Owner: (nill)\n");
 	}
