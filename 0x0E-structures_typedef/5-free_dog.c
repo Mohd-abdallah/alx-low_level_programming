442437407909945651e8633e495b00a6da9f99ec
Mohd-Abdallah <dawry798@gmail.com>
2023-10-12 02:09:35 +0200

    Task No.5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  a function that frees dogs.
+ * @d: pointer
+ * Return: Void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
