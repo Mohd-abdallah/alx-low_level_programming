d1d630cca2afb079495b13d164531c9fa24fd388
Mohd-Abdallah <dawry798@gmail.com>
2023-10-12 03:05:32 +0200

    Task No.0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Standard Header Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+/* Prototypes */
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+#endif
