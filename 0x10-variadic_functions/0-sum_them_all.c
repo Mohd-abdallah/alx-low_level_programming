f1b44feff568e6a9c799684b5f8ae75fac1fb878
Mohd-Abdallah <dawry798@gmail.com>
2023-10-12 16:19:39 +0200

    Fix Task No.0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 		va_start(args, n);
 		for (i = 0, i < n; i++)
 		{
-			int sum += va_args(args, int);
+			sum += va_args(args, int);
 		}
 	}
 	return (sum);
