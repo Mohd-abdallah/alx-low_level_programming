4dd599bb8516a22c2b686665028d65cadce53be4
Mohd-abdallah <dawry798@gmail.com>
2023-10-25 00:54:34 +0200

    Fix - Task No.1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,10 +7,8 @@
  */
 size_t listint_len(const listint_t *h)
 {
-		size_t num_of_element = 0;
+	size_t num_of_element = 0;
 
-	if (h == NULL)
-		return (-1);
 	while (h != NULL)
 	{
 		num_of_element++;
