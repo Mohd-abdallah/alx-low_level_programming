4fede31b0066d61684bc8b7a948b4505c944b8d7
Mohd-abdallah <dawry798@gmail.com>
2023-10-30 23:58:31 +0200

    Fix 5 Task No.3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,22 +4,19 @@
 #include <fcntl.h>
 #include <string.h>
 
-/* Macro */
 #define BUFFER_SIZE 1024
 
-/* Prototypes */
-int test_func(int ac, char **av);
+int test(int ac, char **av);
 int copyFile(const char *source, const char *destination);
 
 
 /**
- * test_func - function that test
+ * test - function that test
  * @ac: number of arguments.
  * @av: arguments.
  * Return: 0 on success, 1 on failure.
  */
-
-int test_func(int ac, char **av)
+int test(int ac, char **av)
 {
 	if (ac != 3)
 	{
@@ -36,53 +33,51 @@ int test_func(int ac, char **av)
 
 /**
  * copyFile - function that copies a file.
- * @source: copy sourse
- * @destination: copy destination
+ * @source: copy from
+ * @destination: copy to
  * Return: 0 on success.
  */
-
 int copyFile(const char *source, const char *destination)
 {
-	int file_one, file_two, read_file, write_file;
+	int f1, f2, r1, w2;
 	char buffer[BUFFER_SIZE];
 
-	file_one = open(source, O_RDONLY);
-	if (file_one == -1)
+	f1 = open(source, O_RDONLY);
+	if (f1 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
 		return (1);
 	}
-	read_file = read(file_one, buffer, sizeof(buffer));
-	if (read_file == -1)
+	r1 = read(f1, buffer, sizeof(buffer));
+	if (r1 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", source);
-		close(file_one);
+		close(f1);
 		return (1);
 	}
-	file_two = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_two == -1)
+	f2 = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f2 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
-		close(file_one);
+		close(f1);
 		return (1);
 	}
-	write_file = write(file_two, buffer, read_file);
-	if (write_file == -1)
+	w2 = write(f2, buffer, r1);
+	if (w2 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't write to file %s\n", destination);
-		close(file_one);
-		close(file_two);
+		close(f1);
+		close(f2);
 		return (1);
 	}
-	if (close(file_one) == -1)
+	if (close(f1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 		perror("close");
 		return (1);
 	}
 	return (0);
 }
-
 
 /**
  * main - function that copies a file.
@@ -92,7 +87,7 @@ int copyFile(const char *source, const char *destination)
  */
 int main(int ac, char **av)
 {
-	if (test_func(ac, av) != 0)
+	if (test(ac, av) != 0)
 	{
 		exit(1);
 	}
